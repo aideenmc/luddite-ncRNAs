cc78b3a6166596bb26311a096a6e3466712c9fcc
Aideen McCabe <aideen.mccabe@ucdconnect.ie>
2021-06-09 14:34:08 +0100

    fastqc quality reports added

---
+++ code/download_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while getopts f:c:n: flag
+do
+	case "${flag}" in
+		f) FILENAME="${OPTARG:-datasets.txt}";;
+		c) CREDENTIALS="${OPTARG:-CREDENTIALS_FILE.json}";;
+		n) N_PARALLEL="${OPTARG:-1}";;
+	esac
+done
+
+LINES=$(cat $FILENAME)
+
+for LINE in $LINES
+do
+	pyega3 -cf $CREDENTIALS -c $N_PARALLEL fetch "$LINE"
+done
