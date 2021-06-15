8fd5d467482f8c1b206804c701409eff36f8fe4e
Aideen McCabe <aideen.mccabe@ucdconnect.ie>
2021-06-15 22:35:18 +0100

    Small modifications to the downloader shell script to ensure compatibility

--- code/downloader.sh
+++ code/downloader.sh
@@ -2,7 +2,7 @@
 
 while getopts f:c:n: flag
 do
-	case "${flag}" in
+	case "$flag" in
 		f) FILENAME="${OPTARG:-datasets.txt}";;
 		c) CREDENTIALS="${OPTARG:-CREDENTIALS_FILE.json}";;
 		n) N_PARALLEL="${OPTARG:-1}";;
@@ -10,8 +10,6 @@ do
 done
 
 LINES=$(cat $FILENAME)
-
-for LINE in $LINES
-do
-	pyega3 -cf $CREDENTIALS -c $N_PARALLEL fetch "$LINE"
+for LINE in $LINES; do
+	pyega3 -cf $CREDENTIALS -c $N_PARALLEL fetch $LINE
 done
